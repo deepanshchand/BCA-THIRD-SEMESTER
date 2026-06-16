a41224ad8a8c148ffd889bf60cf28dc84e7f706c
Deepansh Chand <deepanshchand858@gmail.com>
2026-06-17 04:01:39 +0530

    Add File

--- LAB-2/12.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int num;
-    printf("Enter num : ");
-    scanf("%d", &num);
-    (num % 2 == 0) ? printf("%d is even number", num) : printf("%d is odd number", num);
-    return 0;
-}
